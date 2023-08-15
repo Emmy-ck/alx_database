03f03cb4364f22f51f1186e11e6794929e6a7e37
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-16 02:15:17 +0300

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- script that creates a new second table in the database--
-USE hbtn_0c_0;
-CREATE IF NOT EXISTS TABLE second_table (id INT, name VARCHAR(256), score INT);
+USE hbtn_test_db_9
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table (id, name, score)
     VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
