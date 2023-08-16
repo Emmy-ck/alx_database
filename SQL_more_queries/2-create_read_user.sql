025a9a19c01a8478a97473a8370f45617b14201d
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-17 02:10:10 +0300

    Read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- A script that creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_02_2_pwd';
+-- grants select privilege to user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
