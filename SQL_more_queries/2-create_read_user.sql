f3337736c9c25a7c5e44d69583561bf210e01146
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-17 02:11:12 +0300

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- A script that creates a database
+--Script that creates a database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create a user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_02_2_pwd';
