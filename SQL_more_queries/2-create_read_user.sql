a27024c60b5044e7e622d76243db975566d9d2bf
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-17 02:16:56 +0300

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
---Script that creates a database if it does not exist
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
---create a user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_02_2_pwd';
---grants select privilege to user
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to the user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
