7fa0e7f6f15c0e2b0762724c844b2198ed03002b
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-17 03:15:38 +0300

    Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
---Script that creates a table on my server
-USE hbtn_test_db_3;
+-- create a table in your sql server
+
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
