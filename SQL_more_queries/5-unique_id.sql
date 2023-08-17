5e0193ae3e66334e86c7ef3ae8851f1509416bbb
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-17 04:32:38 +0300

    Unique ID

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,2 @@
 --Script that creates a table 
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT UNIQUE DEFAULT 1,
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
