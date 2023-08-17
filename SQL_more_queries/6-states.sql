a60aa549b53c504f68a7df712658c310d95ed26b
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-17 23:54:51 +0300

    Task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,13 @@
---Create a table in a database 
-CREATE IF NOT EXISTS hbtn_0d_usa;
+--script that creates the database hbtn_0d_usa and
+-- Create the database.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database as current.
 USE hbtn_0d_usa;
---Create a table in the selected database 
-CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
+
+-- Create the table.
+CREATE TABLE IF NOT EXISTS states
+(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
