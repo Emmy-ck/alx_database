9893c2c1e1ac489492c024292f4d1f9072330a7e
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-12 13:12:44 +0300

    Task 4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,2 @@
 --Script that ceates a table 
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
-CREATE TABLE IF NOT EXISTS id_not_null 
-(id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL, name VARCHAR(256));
